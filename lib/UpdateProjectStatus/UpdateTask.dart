2018d3275eb5a7f345f4b78c609a6938d40b6ac7
prateekpm123 <prateekpm123@gmail.com>
2020-08-22 14:05:04 +0530

    All changes made

--- lib/UpdateProjectStatus/UpdateTask.dart
+++ lib/UpdateProjectStatus/UpdateTask.dart
@@ -16,7 +16,7 @@ class UpdateTask extends StatefulWidget {
 
 class _UpdateTaskState extends State<UpdateTask> {
   final databaseReference = FirebaseDatabase.instance.reference();
-  String taskName, taskDescription;
+  String taskName, statusDescription;
   double statusProgress;
   List taskDetails;
   String dropdownValue = "Not Started";
@@ -37,61 +37,56 @@ class _UpdateTaskState extends State<UpdateTask> {
         .child("projects")
         .child(widget.projectID)
         .child("tasks")
+        .child(widget.taskID["taskID"])
         .once()
         .then((DataSnapshot snapshot) {
       setState(() {
-        taskName = snapshot.value[widget.taskID["taskID"]]["taskID"].toString();
-        // taskDescription = snapshot.value["taskDescription"].toString();
+        taskName = snapshot.value["taskID"].toString();
+        // statusDescription = snapshot.value["statusDescription"].toString();
         // statusProgress = snapshot.value["progress"];
       });
     });
 
     debugPrint("this is just a taste" + taskName);
-    debugPrint(taskDescription);
+    debugPrint(statusDescription);
     debugPrint(statusProgress.toString());
   }
 
   updateTask() async {
-    // if (_formKey.currentState.validate()) {
-    setState(() {
-      // taskName = taskNameController.text;
-      taskDescription = statusDescriptionController.text;
-      // hours = hoursController.text;
-    });
-
-    if (taskDescription == "" || taskDescription == null)
-      taskDescription = "not specified";
-    else
-      taskDescription = taskDescription;
-
-    // if (hours == "" || hours == null)
-    //   hours = "not specified";
-    // else
-    //   hours = hours;
-
-    var uuid = Uuid();
-    String uniqueID = uuid.v1();
-
-    // final DateTime now = DateTime.now();
-    // final DateFormat formatter = DateFormat('yyyy-MM-dd hh:mm');
-    // final String requestTime = formatter.format(now);
-
-    try {
-      databaseReference
-          .child("projects")
-          .child(widget.projectID)
-          .child("tasks")
-          .child(widget.taskID["taskID"])
-          .update({
-        'taskDescription': taskDescription,
-        'status': dropdownValue,
-        'progress': _currentSliderValue,
+    if (_formKey.currentState.validate()) {
+      setState(() {
+        // taskName = taskNameController.text;
+        statusDescription = statusDescriptionController.text;
+        // hours = hoursController.text;
       });
-      showToast("Edited \nSuccessfully");
-    } catch (e) {
-      showToast("Failed. check your internet!");
+
+      if (statusDescription == "" || statusDescription == null)
+        statusDescription = "not specified";
+      else
+        statusDescription = statusDescription;
+
+      var uuid = Uuid();
+      String uniqueID = uuid.v1();
+      try {
+        databaseReference
+            .child("projects")
+            .child(widget.projectID)
+            .child("tasks")
+            .child(widget.taskID["taskID"])
+            .update({
+          'statusDescription': statusDescription,
+          'status': dropdownValue,
+          'progress': _currentSliderValue,
+        });
+        showToast("Edited \nSuccessfully");
+      } catch (e) {
+        showToast("Failed. check your internet!");
+      }
     }
-    // }
+  }
+
+  String format(double n) {
+    return n.toStringAsFixed(n.truncateToDouble() == n ? 0 : 0);
   }
 
   @override
@@ -102,7 +97,9 @@ class _UpdateTaskState extends State<UpdateTask> {
     setState(() {
       taskNameController.text = widget.taskID["taskName"].toString();
       statusDescriptionController.text =
-          widget.taskID["taskDescription"].toString();
+          widget.taskID["statusDescription"].toString();
+      dropdownValue = widget.taskID["status"].toString();
+      _currentSliderValue = widget.taskID["progress"].toDouble();
     });
   }
 
@@ -110,173 +107,200 @@ class _UpdateTaskState extends State<UpdateTask> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Update Tasks"),
+        title: Text("Update Work Details"),
       ),
       body: Container(
         child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    "Task Name: " + widget.taskID["taskName"],
-                    overflow: TextOverflow.clip,
-                    maxLines: 1,
-                    softWrap: false,
-                    style: TextStyle(fontSize: 25),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    "Task Desc: " + widget.taskID["taskDescription"],
-                    overflow: TextOverflow.clip,
-                    maxLines: 3,
-                    softWrap: false,
-                    style: TextStyle(fontSize: 23),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    "Start Date:" + widget.taskID["duration"]["startDate"],
-                    overflow: TextOverflow.clip,
-                    maxLines: 3,
-                    softWrap: false,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    "End Date:" + widget.taskID["duration"]["endDate"],
-                    overflow: TextOverflow.clip,
-                    maxLines: 3,
-                    softWrap: false,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              // TextFormField(
-              //   minLines: 1,
-              //   maxLines: 2,
-              //   validator: (String content) {
-              //     if (content.length == 0) {
-              //       return "Please enter task name";
-              //     } else {
-              //       return null;
-              //     }
-              //   },
-              //   controller: taskNameController,
-              //   decoration: InputDecoration(
-              //     labelText: "Task Name",
-              //     border: OutlineInputBorder(),
-              //   ),
-              // ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                minLines: 1,
-                maxLines: 2,
-                validator: (String content) {
-                  if (content.length == 0) {
-                    return "Please enter task name";
-                  } else {
-                    return null;
-                  }
-                },
-                // onChanged: (content) {
-                //   debugPrint("tesing on changes it changes or not");
-                //   updateTask(content);
-                // },
-                controller: statusDescriptionController,
-                decoration: InputDecoration(
-                  labelText: "Status Description",
-                  border: OutlineInputBorder(),
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 10,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              DropdownButton<String>(
-                value: dropdownValue,
-                icon: Icon(Icons.arrow_downward),
-                iconSize: 24,
-                elevation: 16,
-                hint: Text('What is the work Status '),
-                style: TextStyle(color: Colors.black),
-                underline: Container(
-                  height: 2,
-                  color: Colors.blue[300],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Task Name: " + widget.taskID["taskName"],
+                      overflow: TextOverflow.clip,
+                      maxLines: 1,
+                      softWrap: false,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
                 ),
-                onChanged: (String newValue) {
-                  setState(() {
-                    dropdownValue = newValue;
-                  });
-                },
-                items: <String>['Not Started', 'On Going', 'Completed', 'Stuck']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-
-              Text(
-                _currentSliderValue.toString(),
-                style: TextStyle(fontSize: 20),
-              ),
-              Slider(
-                value: _currentSliderValue,
-                min: 0,
-                max: 100,
-                divisions: 100,
-                label: _currentSliderValue.round().toString(),
-                onChanged: (double value) {
-                  setState(() {
-                    _currentSliderValue = value;
-                  });
-                },
-              ),
-
-              RaisedButton(
-                  color: Colors.amber[300],
-                  onPressed: () {
-                    updateTask();
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Task Desc: " + widget.taskID["taskDescription"],
+                      overflow: TextOverflow.clip,
+                      maxLines: 3,
+                      softWrap: false,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Start Date:" + widget.taskID["duration"]["startDate"],
+                      overflow: TextOverflow.clip,
+                      maxLines: 3,
+                      softWrap: false,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "End Date:" + widget.taskID["duration"]["endDate"],
+                      overflow: TextOverflow.clip,
+                      maxLines: 3,
+                      softWrap: false,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  "Update Work Details",
+                  style: TextStyle(fontSize: 23),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text("Status"),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: DropdownButton<String>(
+                    isExpanded: true,
+                    value: dropdownValue,
+                    icon: Icon(Icons.arrow_downward),
+                    iconSize: 24,
+                    elevation: 16,
+                    hint: Text('What is the work Status '),
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 19,
+                    ),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.blue[300],
+                    ),
+                    onChanged: (String newValue) {
+                      setState(() {
+                        dropdownValue = newValue;
+                      });
+                    },
+                    items: <String>[
+                      'Not Started',
+                      'On Going',
+                      'Completed',
+                      'Stuck'
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Progress : ",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Text(
+                        format(_currentSliderValue).toString() + "%",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+                Slider(
+                  value: _currentSliderValue,
+                  min: 0,
+                  max: 100,
+                  divisions: 100,
+                  label: _currentSliderValue.round().toString(),
+                  onChanged: (double value) {
+                    setState(() {
+                      _currentSliderValue = value;
+                    });
                   },
-                  child: Text("Update")),
-              SizedBox(
-                height: 20,
-              ),
-            ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: TextFormField(
+                    minLines: 1,
+                    maxLines: 3,
+                    validator: (String content) {
+                      if (content.length == 0) {
+                        return "Please enter task name";
+                      } else {
+                        return null;
+                      }
+                    },
+                    // onChanged: (content) {
+                    //   debugPrint("tesing on changes it changes or not");
+                    //   updateTask(content);
+                    // },
+                    controller: statusDescriptionController,
+                    decoration: InputDecoration(
+                      labelText: "Status Description",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                RaisedButton(
+                    color: Colors.amber[300],
+                    onPressed: () {
+                      updateTask();
+                    },
+                    child: Text("Update Details")),
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
           ),
         ),
       ),
