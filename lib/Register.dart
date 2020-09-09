1bbe8ef2453beaaff873c75c21662f01e6440e32
prateekpm123 <prateekpm123@gmail.com>
2020-09-09 21:21:43 +0530

    Registration Page Done and working

---
+++ lib/Register.dart
@@ -0,0 +1,299 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:project_timeline/CommonWidgets.dart';
+import 'package:uuid/uuid.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final databaseReference = FirebaseDatabase.instance.reference();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  String _requestType = null ?? "user";
+  String name;
+  String email;
+  String phoneNo;
+  String address;
+  String age;
+  String password;
+
+  TextEditingController controllerName;
+  TextEditingController controllerEmail;
+  TextEditingController controllerPhoneNo;
+  TextEditingController controllerAddress;
+  TextEditingController controllerAge;
+  TextEditingController controllerPassword;
+
+  addUser() async {
+    debugPrint(name);
+    if (_formKey.currentState.validate()) {
+      var uuid = Uuid();
+      String uniqueID = uuid.v1();
+
+      try {
+        await databaseReference
+            .child("request")
+            .child(_requestType)
+            .child(uniqueID)
+            .set({
+          'name': name,
+          'email': email,
+          'phoneNo': phoneNo,
+          'address': address,
+          'age': age,
+          'password': password,
+        });
+        await showToast("User requested Added");
+
+        await setState(() {
+          controllerAddress = null;
+          controllerName = null;
+          controllerEmail = null;
+          controllerPhoneNo = null;
+          controllerAge = null;
+          controllerPassword = null;
+        });
+
+        Navigator.pop(context);
+      } catch (e) {
+        showToast("Failed. Check your Internet !");
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: ThemeAppbar("Request Login"),
+      body: Container(
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Text(
+                        'Add a new machine',
+                        style: titlestyles(18, Colors.orange),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Name",
+                        fillColor: Colors.white,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.blue, width: 2.0),
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(10),
+                              topLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10),
+                              bottomLeft: Radius.circular(10)),
+                        ),
+                      ),
+                      controller: controllerName,
+                      validator: (val) =>
+                          val.isEmpty ? 'Enter your Name' : null,
+                      onChanged: (val) {
+                        setState(() => name = val);
+                      },
+                    ),
+                    SizedBox(height: 15),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        fillColor: Colors.white,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.blue, width: 2.0),
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(10),
+                              topLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10),
+                              bottomLeft: Radius.circular(10)),
+                        ),
+                      ),
+                      controller: controllerEmail,
+                      validator: (val) => val.isEmpty ? 'Enter Email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(height: 15),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Phone no",
+                        fillColor: Colors.white,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.blue, width: 2.0),
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(10),
+                              topLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10),
+                              bottomLeft: Radius.circular(10)),
+                        ),
+                      ),
+                      controller: controllerPhoneNo,
+                      validator: (val) =>
+                          val.isEmpty ? 'Enter Phone Number' : null,
+                      onChanged: (val) {
+                        setState(() => phoneNo = val);
+                      },
+                    ),
+                    new Radio(
+                      value: "user",
+                      groupValue: _requestType,
+                      onChanged: (value) {
+                        setState(() {
+                          _requestType = value;
+                        });
+                        debugPrint(_requestType.toString());
+                      },
+                    ),
+                    new Text(
+                      'User',
+                      style: new TextStyle(fontSize: 16.0),
+                    ),
+                    new Radio(
+                      value: "supervisor",
+                      groupValue: _requestType,
+                      onChanged: (value) {
+                        setState(() {
+                          _requestType = value;
+                        });
+                        debugPrint(_requestType.toString());
+                      },
+                    ),
+                    new Text(
+                      'Supervisor',
+                      style: new TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    new Radio(
+                      value: "worker",
+                      groupValue: _requestType,
+                      onChanged: (value) {
+                        setState(() {
+                          _requestType = value;
+                        });
+                        debugPrint(_requestType.toString());
+                      },
+                    ),
+                    new Text(
+                      'Worker',
+                      style: new TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Address",
+                        fillColor: Colors.white,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.blue, width: 2.0),
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(10),
+                              topLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10),
+                              bottomLeft: Radius.circular(10)),
+                        ),
+                      ),
+                      controller: controllerAddress,
+                      validator: (val) =>
+                          val.isEmpty ? 'Enter your Address' : null,
+                      onChanged: (val) {
+                        setState(() => address = val);
+                      },
+                    ),
+                    SizedBox(height: 15),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Age",
+                        fillColor: Colors.white,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.blue, width: 2.0),
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(10),
+                              topLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10),
+                              bottomLeft: Radius.circular(10)),
+                        ),
+                      ),
+                      controller: controllerAge,
+                      validator: (val) => val.isEmpty ? 'Enter your Age' : null,
+                      onChanged: (val) {
+                        setState(() => age = val);
+                      },
+                    ),
+                    SizedBox(height: 15),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        fillColor: Colors.white,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.blue, width: 2.0),
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(10),
+                              topLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10),
+                              bottomLeft: Radius.circular(10)),
+                        ),
+                      ),
+                      controller: controllerPassword,
+                      validator: (val) => val.isEmpty ? 'Enter Password' : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    Center(
+                      child: FlatButton(
+                        child: Container(
+                          height: 50,
+                          width: 400,
+                          decoration: BoxDecoration(gradient: gradients()),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 130, top: 15),
+                            child: Text(
+                              'Add Machine',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {
+                          if (_formKey.currentState.validate()) {
+                            addUser();
+                          }
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:project_timeline/Register.dart';
 
 import 'CommonWidgets.dart';
 import 'CommonWidgets.dart';
@@ -30,30 +31,27 @@ class LoginPageState extends State<LoginPage> {
   final userController = TextEditingController();
   final passController = TextEditingController();
 
-  login(){
+  login() {
     if (_email.contains(_users[0]) &&
         _email != (_users[1]) &&
         _email != (_users[2])) {
       Navigator.push(
         context,
-        MaterialPageRoute(
-            builder: (context) => ManagerHomePage()),
+        MaterialPageRoute(builder: (context) => ManagerHomePage()),
       );
     } else if (_email != (_users[0]) &&
         _email.contains(_users[1]) &&
         _email != (_users[2])) {
       Navigator.push(
         context,
-        MaterialPageRoute(
-            builder: (context) => SupervisorHomePage()),
+        MaterialPageRoute(builder: (context) => SupervisorHomePage()),
       );
     } else if (_email != _users[0] &&
         _email != _users[1] &&
         _email.contains(_users[2])) {
       Navigator.push(
         context,
-        MaterialPageRoute(
-            builder: (context) => WorkerHomePage()),
+        MaterialPageRoute(builder: (context) => WorkerHomePage()),
       );
     } else {
       showToast("Invalid Credentials");
@@ -66,130 +64,145 @@ class LoginPageState extends State<LoginPage> {
     return new Scaffold(
         //resizeToAvoidBottomInset: false,
         body: SingleChildScrollView(
-          child: Container(
-            child:Form(
-            key: formKey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 250,
-                  decoration: BoxDecoration(
-                    gradient: gradients(),
-                    borderRadius:
-                        BorderRadius.only(bottomLeft: Radius.circular(100)),
+            child: Container(
+      child: Form(
+        key: formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 250,
+              decoration: BoxDecoration(
+                gradient: gradients(),
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.circular(100)),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.person,
+                    size: 80,
+                    color: Colors.white,
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(
-                        Icons.person,
-                        size: 80,
-                        color: Colors.white,
-                      ),
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 32),
-                        ),
-                      ),
-                    ],
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 32),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: new Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        width: MediaQuery.of(context).size.width / 1.2,
-                        //height: 50,
-                        padding: EdgeInsets.only(
-                            top: 4, left: 16, right: 16, bottom: 7),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(color: Colors.black26, blurRadius: 10),
-                            ]),
-                        child: TextFormField(
-                          controller: userController,
-                          onChanged: (value) {
-                            _email = value;
-                          },
-                          validator: (val) => val.isEmpty ? 'Enter email' : null,
-                          decoration: InputDecoration(
-                            icon: Icon(
-                              Icons.email,
-                              color: Colors.grey,
-                            ),
-                            hintText: 'Email',
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 50,
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width / 1.2,
-                        //height: 50,
-                        padding: EdgeInsets.only(
-                            top: 4, left: 16, right: 16, bottom: 4),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(color: Colors.black26, blurRadius: 10),
-                            ]),
-                        child: TextFormField(
-                          controller: passController,
-                          validator: (val) => val.isEmpty ? 'Enter password' : null,
-                          decoration: InputDecoration(
-                            icon: Icon(
-                              Icons.vpn_key,
-                              color: Colors.grey,
-                            ),
-                            hintText: 'Password',
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 50,
-                      ),
-
-                      FlatButton(
-                        onPressed: () {
-                          if(formKey.currentState.validate())
-                          {
-                            login();
-                          }
-                        },
-                        textColor: Colors.white,
-                        child: Container(
-                          width: 300,
-                          padding:EdgeInsets.all(20),
-                          decoration: new BoxDecoration(
-                              gradient: gradients()
-                          ),
-                          child: Text(
-                            "Login",
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          ))
-    );
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.2,
+                    //height: 50,
+                    padding:
+                        EdgeInsets.only(top: 4, left: 16, right: 16, bottom: 7),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(color: Colors.black26, blurRadius: 10),
+                        ]),
+                    child: TextFormField(
+                      controller: userController,
+                      onChanged: (value) {
+                        _email = value;
+                      },
+                      validator: (val) => val.isEmpty ? 'Enter email' : null,
+                      decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.email,
+                          color: Colors.grey,
+                        ),
+                        hintText: 'Email',
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.2,
+                    //height: 50,
+                    padding:
+                        EdgeInsets.only(top: 4, left: 16, right: 16, bottom: 4),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(color: Colors.black26, blurRadius: 10),
+                        ]),
+                    child: TextFormField(
+                      controller: passController,
+                      validator: (val) => val.isEmpty ? 'Enter password' : null,
+                      decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.vpn_key,
+                          color: Colors.grey,
+                        ),
+                        hintText: 'Password',
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      if (formKey.currentState.validate()) {
+                        login();
+                      }
+                    },
+                    textColor: Colors.white,
+                    child: Container(
+                      width: 300,
+                      padding: EdgeInsets.all(20),
+                      decoration: new BoxDecoration(gradient: gradients()),
+                      child: Text(
+                        "Login",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Register()),
+                      );
+                    },
+                    textColor: Colors.white,
+                    child: Container(
+                      width: 300,
+                      padding: EdgeInsets.all(20),
+                      decoration: new BoxDecoration(gradient: gradients()),
+                      child: Text(
+                        "Register",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    )));
   }
 }
