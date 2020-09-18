3402ed7e4f867957f1645d0263011e45a1ffb2a2
Siddhi-Kadam <63309357+siddhi3199@users.noreply.github.com>
2020-09-18 16:15:19 +0530

    GUI of SMS edited

---
+++ lib/sms/my_twilio.dart
@@ -0,0 +1,10 @@
+import './messages.dart' show Messages;
+
+class MyTwilio {
+  final String _accountSid;
+  final String _authToken;
+
+  const MyTwilio(this._accountSid, this._authToken);
+
+  Messages get messages => Messages(_accountSid, _authToken);
+}
